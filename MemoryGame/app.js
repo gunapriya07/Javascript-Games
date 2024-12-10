294a3d3eed312946fd8bbde8adcfbbedefe43677
gunapriya07 <147796209+gunapriya07@users.noreply.github.com>
2024-12-10 16:42:15 +0530

    day2

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -1,69 +1,109 @@
-const cardArray=[
+const cardArrray = [
     {
-        name:"fries",
-        img:"MemoryGame/Image-files/images/fries.png",
+        name:'fries',
+        img:'Image-files/images/fries.png'
     },
     {
-        name:"cheeseburger",
-        img:"MemoryGame/Image-files/images/cheeseburger.png"
+        name:'cheeseburger',
+        img:'Image-files/images/cheeseburger.png'
     },
     {
-        name:"hotdog",
-        img:"MemoryGame/Image-files/images/hotdog.png"
+        name:'hotdog',
+        img:'Image-files/images/hotdog.png'
     },
     {
-        name:"ice_cream",
-        img:"MemoryGame/Image-files/images/ice-cream.png"
+        name:'ice-cream',
+        img:'Image-files/images/ice-cream.png'
     },
     {
-        name:"pizza",
-        img:"MemoryGame/Image-files/images/pizza.png"
+        name:'milkshake',
+        img:'Image-files/images/milkshake.png'
     },
     {
-        name:"milkshake",
-        img:"MemoryGame/Image-files/images/milkshake.png"
+        name:'pizza',
+        img:'Image-files/images/pizza.png'
     },
     {
-        name:"fries",
-        img:"MemoryGame/Image-files/images/fries.png",
+        name:'fries',
+        img:'Image-files/images/fries.png'
     },
     {
-        name:"cheeseburger",
-        img:"MemoryGame/Image-files/images/cheeseburger.png"
+        name:'cheeseburger',
+        img:'Image-files/images/cheeseburger.png'
     },
     {
-        name:"hotdog",
-        img:"MemoryGame/Image-files/images/hotdog.png"
+        name:'hotdog',
+        img:'Image-files/images/hotdog.png'
     },
     {
-        name:"ice_cream",
-        img:"MemoryGame/Image-files/images/ice-cream.png"
+        name:'ice-cream',
+        img:'Image-files/images/ice-cream.png'
     },
     {
-        name:"pizza",
-        img:"MemoryGame/Image-files/images/pizza.png"
+        name:'milkshake',
+        img:'Image-files/images/milkshake.png'
     },
     {
-        name:"milkshake",
-        img:"MemoryGame/Image-files/images/milkshake.png"
+        name:'pizza',
+        img:'Image-files/images/pizza.png'
     }
-   
 ]
 
-// console.log(cardArray);
-cardArray.sort(()=>0.5-Math.random())
+cardArrray.sort(()=>0.5 - Math.random());
 
-// console.log(cardArray);
-
-const gridDisplay=document.querySelector("#grid");
-console.log(gridDisplay);
-
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard(){
-    for(let i=0;i<10;i++){
-       const card= document.createElement('img');
-       card.setAttribute("src","images/blank.png");
-       console.log(card,i);
+    for(let i=0;i<cardArrray.length;i++){
+        const card = document.createElement('img')
+        card.setAttribute('src','Image-files/images/blank.png')
+        card.setAttribute('data-id',i)
+        card.addEventListener('click',flipCard)
+        gridDisplay.appendChild(card)
+
+
     }
 }
-createBoard();
+createBoard()
+
+function flipCard(){
+   let cardId =  this.getAttribute('data-id')
+   cardsChosen.push(cardArrray[cardId].name)
+   cardsChosenIds.push(cardId)
+   this.setAttribute('src',cardArrray[cardId].img)
+   if(cardsChosen.length == 2){
+    setTimeout(checkMatch,500)
+   }
+}
+
+function checkMatch(){
+    const cards = document.querySelectorAll('img')
+    const optionOneID = cardsChosenIds[0]
+    const optionTwoID = cardsChosenIds[1]
+    if(optionOneID == optionTwoID){
+        alert('You have clicked the same image!')
+    }
+
+    if(cardsChosen[0] == cardsChosen[1]){
+        alert('you found the match')
+        cards[optionOneID].setAttribute('src','Image-files/images/blank.png')
+        cards[optionTwoID].setAttribute('src','Image-files/images/blank.png')
+        cards[optionOneID].removeEventListener('click',flipCard)
+        cards[optionTwoID].removeEventListener('click',flipCard)
+        cardsWon.push(cardsChosen)
+    }else{
+        cards[optionOneID].setAttribute('src','Image-files/images/blank.png')
+        cards[optionTwoID].setAttribute('src','Image-files/images/blank.png')
+        alert('Sorry try again')
+    }
+    resultDisplay.textContent = cardsWon.length;
+    cardsChosen = []
+    cardsChosenIds = []
+    if(cardsWon.length == (cardArrray.length/2)){
+        resultDisplay.innerHTML = 'Congratulations you found all!'
+    }
+}
